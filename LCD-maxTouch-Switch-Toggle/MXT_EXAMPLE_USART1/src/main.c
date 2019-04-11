d13cc82e4aae7cf456dfd8c71f48d45335bd93b2
guipleite <guipleite@gmail.com>
2019-04-11 13:36:15 -0300

    Conta o tempo e bloqueia a tela

---
+++ LCD-maxTouch-Switch-Toggle/MXT_EXAMPLE_USART1/src/main.c
@@ -0,0 +1,369 @@
+#include <asf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "conf_board.h"
+#include "conf_example.h"
+#include "conf_uart_serial.h"
+#include "maquina1.h"
+
+#define MAX_ENTRIES        3
+#define STRING_LENGTH     70
+
+#define USART_TX_MAX_LENGTH     0xff
+
+#define YEAR        2019
+#define MONTH       3
+#define DAY         19
+#define WEEK        12
+#define HOUR        0
+#define MINUTE      0
+#define SECOND      0
+uint32_t hour, minu, seg;
+
+struct ili9488_opt_t g_ili9488_display_opt;
+const uint32_t BUTTON_W = 120;
+const uint32_t BUTTON_H = 150;
+const uint32_t BUTTON_BORDER = 2;
+const uint32_t BUTTON_X = ILI9488_LCD_WIDTH/2;
+const uint32_t BUTTON_Y = ILI9488_LCD_HEIGHT/2;
+	
+static void configure_lcd(void){
+	/* Initialize display parameter */
+	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
+	g_ili9488_display_opt.ul_height = ILI9488_LCD_HEIGHT;
+	g_ili9488_display_opt.foreground_color = COLOR_CONVERT(COLOR_WHITE);
+	g_ili9488_display_opt.background_color = COLOR_CONVERT(COLOR_WHITE);
+
+	/* Initialize LCD */
+	ili9488_init(&g_ili9488_display_opt);
+}
+
+/**
+ * \brief Set maXTouch configuration
+ *
+ * This function writes a set of predefined, optimal maXTouch configuration data
+ * to the maXTouch Xplained Pro.
+ *
+ * \param device Pointer to mxt_device struct
+ */
+static void mxt_init(struct mxt_device *device)
+{
+	enum status_code status;
+
+	/* T8 configuration object data */
+	uint8_t t8_object[] = {
+		0x0d, 0x00, 0x05, 0x0a, 0x4b, 0x00, 0x00,
+		0x00, 0x32, 0x19
+	};
+
+	/* T9 configuration object data */
+	uint8_t t9_object[] = {
+		0x8B, 0x00, 0x00, 0x0E, 0x08, 0x00, 0x80,
+		0x32, 0x05, 0x02, 0x0A, 0x03, 0x03, 0x20,
+		0x02, 0x0F, 0x0F, 0x0A, 0x00, 0x00, 0x00,
+		0x00, 0x18, 0x18, 0x20, 0x20, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x0A, 0x00, 0x00, 0x02,
+		0x02
+	};
+
+	/* T46 configuration object data */
+	uint8_t t46_object[] = {
+		0x00, 0x00, 0x18, 0x18, 0x00, 0x00, 0x03,
+		0x00, 0x00
+	};
+	
+	/* T56 configuration object data */
+	uint8_t t56_object[] = {
+		0x02, 0x00, 0x01, 0x18, 0x1E, 0x1E, 0x1E,
+		0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E,
+		0x1E, 0x1E, 0x1E, 0x1E, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00
+	};
+
+	/* TWI configuration */
+	twihs_master_options_t twi_opt = {
+		.speed = MXT_TWI_SPEED,
+		.chip  = MAXTOUCH_TWI_ADDRESS,
+	};
+
+	status = (enum status_code)twihs_master_setup(MAXTOUCH_TWI_INTERFACE, &twi_opt);
+	Assert(status == STATUS_OK);
+
+	/* Initialize the maXTouch device */
+	status = mxt_init_device(device, MAXTOUCH_TWI_INTERFACE,
+			MAXTOUCH_TWI_ADDRESS, MAXTOUCH_XPRO_CHG_PIO);
+	Assert(status == STATUS_OK);
+
+	/* Issue soft reset of maXTouch device by writing a non-zero value to
+	 * the reset register */
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_COMMANDPROCESSOR_T6, 0)
+			+ MXT_GEN_COMMANDPROCESSOR_RESET, 0x01);
+
+	/* Wait for the reset of the device to complete */
+	delay_ms(MXT_RESET_TIME);
+
+	/* Write data to configuration registers in T7 configuration object */
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_POWERCONFIG_T7, 0) + 0, 0x20);
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_POWERCONFIG_T7, 0) + 1, 0x10);
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_POWERCONFIG_T7, 0) + 2, 0x4b);
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_POWERCONFIG_T7, 0) + 3, 0x84);
+
+	/* Write predefined configuration data to configuration objects */
+	mxt_write_config_object(device, mxt_get_object_address(device,
+			MXT_GEN_ACQUISITIONCONFIG_T8, 0), &t8_object);
+	mxt_write_config_object(device, mxt_get_object_address(device,
+			MXT_TOUCH_MULTITOUCHSCREEN_T9, 0), &t9_object);
+	mxt_write_config_object(device, mxt_get_object_address(device,
+			MXT_SPT_CTE_CONFIGURATION_T46, 0), &t46_object);
+	mxt_write_config_object(device, mxt_get_object_address(device,
+			MXT_PROCI_SHIELDLESS_T56, 0), &t56_object);
+
+	/* Issue recalibration command to maXTouch device by writing a non-zero
+	 * value to the calibrate register */
+	mxt_write_config_reg(device, mxt_get_object_address(device,
+			MXT_GEN_COMMANDPROCESSOR_T6, 0)
+			+ MXT_GEN_COMMANDPROCESSOR_CALIBRATE, 0x01);
+}
+
+void RTC_init(){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(RTC, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(RTC, YEAR, MONTH, DAY, WEEK);
+	rtc_set_time(RTC, HOUR, MINUTE, SECOND);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(RTC_IRQn);
+	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NVIC_SetPriority(RTC_IRQn, 0);
+	NVIC_EnableIRQ(RTC_IRQn);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(RTC,  RTC_IER_ALREN);
+
+}
+
+void draw_screen(void) {
+	ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
+	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+}
+
+volatile bool start = false;
+volatile bool locked = false;
+
+
+void draw_button(uint32_t clicked) {
+	static uint32_t last_state = 255; // undefined
+	uint8_t stingLCD[256];
+
+	if(clicked == last_state) return;
+	
+	ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+	ili9488_draw_filled_rectangle(BUTTON_X-BUTTON_W/2, BUTTON_Y-BUTTON_H/2, BUTTON_X+BUTTON_W/2, BUTTON_Y+BUTTON_H/2);
+	ili9488_set_foreground_color(COLOR_CONVERT(COLOR_TOMATO));
+	ili9488_draw_filled_circle(275, 50,25);
+	if (locked){
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+		ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH, ILI9488_LCD_HEIGHT);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_GREEN));
+		ili9488_draw_filled_circle(275, 50,25);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
+	}
+	else {
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
+		ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH, ILI9488_LCD_HEIGHT);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_TOMATO));
+		ili9488_draw_filled_circle(275, 50,25);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+		ili9488_draw_filled_rectangle(BUTTON_X-BUTTON_W/2, BUTTON_Y-BUTTON_H/2, BUTTON_X+BUTTON_W/2, BUTTON_Y+BUTTON_H/2);
+		
+		if(clicked==1) {
+			ili9488_set_foreground_color(COLOR_CONVERT(COLOR_TOMATO));
+			ili9488_draw_filled_rectangle(BUTTON_X-BUTTON_W/2+BUTTON_BORDER, BUTTON_Y+BUTTON_BORDER, BUTTON_X+BUTTON_W/2-BUTTON_BORDER, BUTTON_Y+BUTTON_H/2-BUTTON_BORDER);
+		
+			RTC_init();
+			start = true;
+		
+			} 
+		if(clicked == 0) {
+			ili9488_set_foreground_color(COLOR_CONVERT(COLOR_GREEN));
+			ili9488_draw_filled_rectangle(BUTTON_X-BUTTON_W/2+BUTTON_BORDER, BUTTON_Y-BUTTON_H/2+BUTTON_BORDER, BUTTON_X+BUTTON_W/2-BUTTON_BORDER, BUTTON_Y-BUTTON_BORDER);
+			start = false;
+			
+		}
+		last_state = clicked;
+	}
+}
+
+uint32_t convert_axis_system_x(uint32_t touch_y) {
+	// entrada: 4096 - 0 (sistema de coordenadas atual)
+	// saida: 0 - 320
+	return ILI9488_LCD_WIDTH - ILI9488_LCD_WIDTH*touch_y/4096;
+}
+
+uint32_t convert_axis_system_y(uint32_t touch_x) {
+	// entrada: 0 - 4096 (sistema de coordenadas atual)
+	// saida: 0 - 320
+	return ILI9488_LCD_HEIGHT*touch_x/4096;
+}
+volatile int c =0;
+volatile int clkd;
+void update_screen(uint32_t tx, uint32_t ty) {
+	if(locked==false){
+		if(tx >= BUTTON_X-BUTTON_W/2 && tx <= BUTTON_X + BUTTON_W/2) {
+			if(ty >= BUTTON_Y-BUTTON_H/2 && ty <= BUTTON_Y) {
+				//draw_button(1);
+				clkd =1;
+			} 
+			else if(ty > BUTTON_Y && ty < BUTTON_Y + BUTTON_H/2) {
+				//draw_button(0);
+				clkd =0;
+			}
+			draw_button(clkd);
+		}
+	}
+	
+	if(tx >=275 && tx <= 315 && ty >= 25 && ty <= 50) {
+		c++;
+		if(c%2!=0){
+			if (locked){
+				locked=false;
+				uint8_t stingLCD[256];
+				sprintf(stingLCD, "%d ",clkd);
+				ili9488_draw_string(40, 400, stingLCD);
+			}
+			else{
+				locked=true;
+			}
+			draw_button(3);
+
+		}
+	}
+	
+}
+
+void mxt_handler(struct mxt_device *device)
+{
+	/* USART tx buffer initialized to 0 */
+	char tx_buf[STRING_LENGTH * MAX_ENTRIES] = {0};
+	uint8_t i = 0; /* Iterator */
+
+	/* Temporary touch event data struct */
+	struct mxt_touch_event touch_event;
+
+	/* Collect touch events and put the data in a string,
+	 * maximum 2 events at the time */
+	do {
+		/* Temporary buffer for each new touch event line */
+		char buf[STRING_LENGTH];
+	
+		/* Read next next touch event in the queue, discard if read fails */
+		if (mxt_read_touch_event(device, &touch_event) != STATUS_OK) {
+			continue;
+		}
+		
+		 // eixos trocados (quando na vertical LCD)
+		uint32_t conv_x = convert_axis_system_x(touch_event.y);
+		uint32_t conv_y = convert_axis_system_y(touch_event.x);
+		
+		/* Format a new entry in the data string that will be sent over USART */
+		sprintf(buf, "Nr: %1d, X:%4d, Y:%4d, Status:0x%2x conv X:%3d Y:%3d\n\r",
+				touch_event.id, touch_event.x, touch_event.y,
+				touch_event.status, conv_x, conv_y);
+		update_screen(conv_x, conv_y);
+
+		/* Add the new string to the string buffer */
+		strcat(tx_buf, buf);
+		i++;
+
+		/* Check if there is still messages in the queue and
+		 * if we have reached the maximum numbers of events */
+	} while ((mxt_is_message_pending(device)) & (i < MAX_ENTRIES));
+
+	/* If there is any entries in the buffer, send them over USART */
+	if (i > 0) {
+		usart_serial_write_packet(USART_SERIAL_EXAMPLE, (uint8_t *)tx_buf, strlen(tx_buf));
+	}
+}
+void clear_LCD(int a, int b){
+	if (locked){
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+		ili9488_draw_filled_rectangle(0, a, ILI9488_LCD_WIDTH-1, b);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
+	}
+	else{
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_WHITE));
+		ili9488_draw_filled_rectangle(0, a, ILI9488_LCD_WIDTH-1, b);
+		ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+	}
+}
+
+void crono(int tempo){
+	clear_LCD(390,415);
+	if (start==true){
+		uint8_t stingLCD[256];
+		
+		rtc_get_time(RTC, &hour, &minu, &seg);
+		sprintf(stingLCD, "%d :%d de %d min.",minu,seg,tempo);
+		ili9488_draw_string(40, 400, stingLCD);
+
+		if(minu == tempo){
+			sprintf(stingLCD, "ACABOU A LAVAGEM SEU CORNO");
+			ili9488_draw_string(5, 350, stingLCD);
+			start=false;
+		}
+	}
+	else{return;}
+}
+
+
+int main(void)
+{
+	struct mxt_device device; /* Device data container */
+
+	/* Initialize the USART configuration struct */
+	const usart_serial_options_t usart_serial_options = {
+		.baudrate     = USART_SERIAL_EXAMPLE_BAUDRATE,
+		.charlength   = USART_SERIAL_CHAR_LENGTH,
+		.paritytype   = USART_SERIAL_PARITY,
+		.stopbits     = USART_SERIAL_STOP_BIT
+	};
+
+	sysclk_init(); /* Initialize system clocks */
+	board_init();  /* Initialize board */
+	configure_lcd();
+	draw_screen();
+	draw_button(0);
+	/* Initialize the mXT touch device */
+	mxt_init(&device);
+	
+	/* Initialize stdio on USART */
+	stdio_serial_init(USART_SERIAL_EXAMPLE, &usart_serial_options);
+
+	printf("\n\rmaXTouch data USART transmitter\n\r");
+		
+
+	while (true) {
+		delay_s(1);
+		/* Check for any pending messages and run message handler if any
+		 * message is found in the queue */
+		if (mxt_is_message_pending(&device)) {
+			mxt_handler(&device);
+		}
+		crono(1);
+		
+	}
+
+	return 0;
+}
